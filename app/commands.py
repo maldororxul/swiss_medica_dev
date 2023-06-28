fc558cbe83b68dd4661ffbbc2a1be663913f8145
maldororxul <studia-kirill@yandex.ru>
2023-06-28 14:46:45 +0400

    commands

--- app/commands.py
+++ app/commands.py
@@ -1,6 +1,9 @@
 import click
 from flask.cli import with_appcontext
+from sqlalchemy import create_engine
+from sqlalchemy.sql.ddl import CreateSchema
 
+from config import Config
 from .extensions import db
 from .models.amo_credentials import CDVAmoCredentials, SMAmoCredentials
 from .models.amo_token import SMAmoToken, CDVAmoToken
@@ -19,4 +22,13 @@ from .models.log import SMLog, CDVLog
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
+    engine = create_engine(
+        Config.SQLALCHEMY_DATABASE_URI,
+        pool_size=20,
+        max_overflow=100,
+        pool_pre_ping=True
+    )
+    for schema_name in ('sm', 'cdv'):
+        if not engine.dialect.has_schema(engine, schema_name):
+            engine.execute(CreateSchema(schema_name))
     db.create_all()
