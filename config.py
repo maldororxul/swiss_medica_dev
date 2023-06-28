de5c047c7a68feccf64c5476755a23310ece6b07
maldororxul <studia-kirill@yandex.ru>
2023-06-28 14:14:50 +0400

    wsgidb config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
     CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
     CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND')
