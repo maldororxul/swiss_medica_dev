844b5ad22f3e6cb5aa5927efb47ea29f22a9754a
maldororxul <studia-kirill@yandex.ru>
2023-07-03 16:38:52 +0400

    connections owerflow

--- app/engine.py
+++ app/engine.py
@@ -1,11 +1,20 @@
+import os
+
 from sqlalchemy import create_engine
 from config import Config
 
+CONNECTIONS_LIMIT = os.environ.get('CONNETIONS_LIMIT')
+
+engine = None
+
 
 def get_engine():
-    return create_engine(
-        Config.SQLALCHEMY_DATABASE_URI,
-        pool_size=3,
-        max_overflow=6,
-        pool_pre_ping=True
-    )
+    global engine
+    if engine is None:
+        engine = create_engine(
+            Config.SQLALCHEMY_DATABASE_URI,
+            pool_size=CONNECTIONS_LIMIT,
+            max_overflow=CONNECTIONS_LIMIT,
+            pool_pre_ping=True
+        )
+    return engine
