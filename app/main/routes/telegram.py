803aa63d13b7f8b334b5010596edfb335aed71ee
maldororxul <studia-kirill@yandex.ru>
2023-07-14 15:11:02 +0400

    telegram send_msg

--- app/main/routes/telegram.py
+++ app/main/routes/telegram.py
@@ -36,6 +36,7 @@ def reply_on_new_lead(_request, msg_builder: Callable):
     else:
         params = Config.NEW_LEAD_TELEGRAM.get(branch)
         bot_key = branch
+    print('>>>', params)
     if not params:
         return 'Bot not found', 404
     return redirect(url_for(
@@ -71,7 +72,6 @@ def test_telegram():
 def send_message(bot_key, chat_id, message):
     if bot_key not in BOTS:
         return 'Bot not found', 404
-    print(f'Bot key: {bot_key}, Chat id: {chat_id}, msg: {message}')
     BOTS[bot_key].send_message(chat_id, message)
     return 'success', 200
 
