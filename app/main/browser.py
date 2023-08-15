fbd6143a6f170c491152c65e951db618c5439f92
maldororxul <studia-kirill@yandex.ru>
2023-08-15 12:58:43 +0400

    browser rollback

--- app/main/browser.py
+++ app/main/browser.py
@@ -2,12 +2,12 @@
 __author__ = 'ke.mizonov'
 import json
 import os
-import threading
+# import threading
 from typing import Optional, List, Dict
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
-from selenium.webdriver import DesiredCapabilities
+# from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
@@ -20,22 +20,22 @@ WAIT_TIME = 60
 
 class KmBrowser:
     """ Selenium и все, что связано с управлением браузером """
-    _instance = None
-    _lock = threading.Lock()  # This will be the lock to ensure one function at a time can use the browser
+    # _instance = None
+    # _lock = threading.Lock()  # This will be the lock to ensure one function at a time can use the browser
 
     def __init__(self, hidden: bool = True):
         self.browser = self.start_browser(hidden=hidden)
 
-    def __new__(cls, *args, **kwargs):
-        with cls._lock:
-            if not cls._instance:
-                cls._instance = super().__new__(cls)
-        return cls._instance
+    # def __new__(cls, *args, **kwargs):
+    #     with cls._lock:
+    #         if not cls._instance:
+    #             cls._instance = super().__new__(cls)
+    #     return cls._instance
 
     def close(self):
         """ Закрывает браузер """
-        with self._lock:
-            self.browser.close()
+        # with self._lock:
+        self.browser.close()
 
     def find_element_by_selector(self, selector: str, timeout: int = WAIT_TIME) -> Optional[WebElement]:
         """ Пытается найти элемент по CSS-селектору
@@ -126,8 +126,8 @@ class KmBrowser:
         Args:
             url: адрес сайта
         """
-        with self._lock:
-            self.browser.get(url=url)
+        # with self._lock:
+        self.browser.get(url=url)
 
     def wait(self, selector: str, timeout: int = WAIT_TIME) -> bool:
         """ Ждем прогрузки всех элментов, удовлетворяющих селектору
