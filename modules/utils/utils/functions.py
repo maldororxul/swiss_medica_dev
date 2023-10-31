1a5a810afb1faf9d2a936af327e5143d050a00d3
maldororxul <studia-kirill@yandex.ru>
2023-11-01 00:53:58 +0400

    tawk offline form data

--- modules/utils/utils/functions.py
+++ modules/utils/utils/functions.py
@@ -1,12 +1,8 @@
 """ Общие утилиты и функции """
 __author__ = 'ke.mizonov'
-
 import os
-import zipfile
 from datetime import datetime
 from typing import Union, List, Dict, Tuple, Optional
-
-from PyQt6.QtCore import QDateTime
 
 
 def append_unique(source_collection: List[Dict], new_collection: List[Dict]):
@@ -62,18 +58,6 @@ def get_current_timeshift() -> int:
     return hours if current_timeshift[0] == '+' else -hours
 
 
-def get_datetime_from_qdatetime(widget: QDateTime()) -> datetime:
-    """ Корректно возвращает дату-время из виджета QDateTime
-
-    Args:
-        widget: виджет даты и времени
-
-    Returns:
-        значение даты и времени
-    """
-    return datetime.strptime(widget.text(), '%d.%m.%Y %H:%M')
-
-
 def get_time_from_str_hh_mm(text: str) -> datetime.time:
     """ Преобразует время из строки в datetime.time
 
@@ -84,21 +68,3 @@ def get_time_from_str_hh_mm(text: str) -> datetime.time:
         время в питоньем формате
     """
     return datetime.strptime(text, '%H:%M').time()
-
-
-def zip_files(name: str, path: str, files: Tuple):
-    """ Запаковывает список файлов в zip-архив (нерекурсивно)
-
-    Args:
-        name: имя архива
-        path: путь к файлам
-        files: список файлов
-    """
-    full_name = os.path.join(path, f'{name}.zip')
-    try:
-        os.remove(full_name)
-    except:
-        pass
-    with zipfile.ZipFile(full_name, 'a') as myzip:
-        for file in files:
-            myzip.write(file)
