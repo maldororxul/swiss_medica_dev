29f34c424f65d4ec622c6b86831aa90f0e71ebf4
maldororxul <studia-kirill@yandex.ru>
2023-12-15 02:27:12 +0400

    bwa threading

--- app/main/routes/whatsapp.py
+++ app/main/routes/whatsapp.py
@@ -6,7 +6,7 @@ import uuid
 from threading import Thread
 from typing import Optional, List, Dict
 import requests
-from flask import request, jsonify
+from flask import request, jsonify, current_app
 
 from app.amo.api.chat_client import AmoChatsAPIClient
 from app.amo.processor.functions import clear_phone
@@ -141,156 +141,158 @@ def whatsapp_webhook():
 def process_whatsapp_message(data: Dict):
     # Здесь ваш код для обработки данных, например, загрузка файла
     print('incoming WhatsApp data:', data)
-    """
-    {'profile': {'name': 'Kirill Mizonow'}, 'wa_id': '995591058618'}], 'messages': [{'from': '995591058618', 'id': 'wamid.HBgMOTk1NTkxMDU4NjE4FQIAEhggQkFDNTcwN0VGMzY1RDEyNUZBQTcxRDZBM0U5QjE4OTMA', 'timestamp': '1700943407', 'text': {'body': 'Сообщение отправлено из WhatsApp... 2'}, 'type': 'text'}]}, 'field': 'messages'}]}]}
-    """
-    try:
-        entry = data['entry'][0]
-        value = entry['changes'][0]['value']
-        # определяем идентификатор нашего номера, на который пришло сообщение
-        phone_number_id = value['metadata']['phone_number_id']
-        # по идентификатору номера определяем филиал
-        branch = None
-        for _branch, config in Config().whatsapp.items():
-            for number in config.get('numbers'):
-                if number['id'] == phone_number_id:
-                    branch = _branch
+    app = current_app._get_current_object()
+    with app.app_context():
+        """
+        {'profile': {'name': 'Kirill Mizonow'}, 'wa_id': '995591058618'}], 'messages': [{'from': '995591058618', 'id': 'wamid.HBgMOTk1NTkxMDU4NjE4FQIAEhggQkFDNTcwN0VGMzY1RDEyNUZBQTcxRDZBM0U5QjE4OTMA', 'timestamp': '1700943407', 'text': {'body': 'Сообщение отправлено из WhatsApp... 2'}, 'type': 'text'}]}, 'field': 'messages'}]}]}
+        """
+        try:
+            entry = data['entry'][0]
+            value = entry['changes'][0]['value']
+            # определяем идентификатор нашего номера, на который пришло сообщение
+            phone_number_id = value['metadata']['phone_number_id']
+            # по идентификатору номера определяем филиал
+            branch = None
+            for _branch, config in Config().whatsapp.items():
+                for number in config.get('numbers'):
+                    if number['id'] == phone_number_id:
+                        branch = _branch
+                        break
+                if branch:
                     break
-            if branch:
-                break
-        if not branch:
-            # print('no branch')
-            return '200 OK HTTPS.', 200
-        contact = (value.get('contacts') or [{}])[0]
-        if not contact:
-            """
-            {'object': 'whatsapp_business_account', 'entry': [{'id': '133570986506988', 'changes': [{'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '381114221400', 'phone_number_id': '151648284687808'}, 'statuses': [{'id': 'wamid.HBgMOTk1NTkxMDU4NjE4FQIAERgSOEJDNTQzN0E1MkU2QzEwM0UwAA==', 'status': 'delivered', 'timestamp': '1701896963', 'recipient_id': '995591058618', 'conversation': {'id': '284a2cc182835831700f6711b563f032', 'origin': {'type': 'utility'}}, 'pricing': {'billable': True, 'pricing_model': 'CBP', 'category': 'utility'}}]}, 'field': 'messages'}]}]}
-            """
-            # print('uncatched WhatsApp error')
-            return '200 OK HTTPS.', 200
-        msg = value['messages'][0]
-        # print('msg', msg)
-        timestamp = int(msg['timestamp'])
-        name = contact['profile']['name']
-        phone = contact['wa_id']
-        # текста может не быть - вероятно, нам прислали файл
-        text = (msg.get('text') or {}).get('body')
-        # ищем лид и связанный с ним контакт
-        contact_id = None
-        # print('init amo api client...')
-        amo_client = API_CLIENT.get(branch)()
-        # print('searching contacts...', phone[-8:])
-        contacts = [x for x in amo_client.find_contacts(query=phone[-8:]) or []]
-        if contacts:
-            contact_id = contacts[0]['id']
-            # print('contact found!', contacts)
-        # leads = [x for x in amo_client.find_leads(query=phone[-8:])]
-        # print('leads', leads)
-        # if leads:
-        #     contacts = leads[0]['_embedded']['contacts']
-        #     if contacts:
-        #         contact_id = contacts[0]['id']
-        # если контакт существует, ищем связанные с ним чаты
-        chat_id = None
-        conversation_id = None
-        if contact_id:
-            print('trying to find chats...')
-            chats = amo_client.get_chats(contact_id=contact_id)
-            print('chats', chats)
-            if chats:
-                chat_id = chats[-1]['chat_id']
-        else:
-            # контакта не существует, кидаем чат в "неразобранное"
-            # conversation_id = str(uuid.uuid4())
-            # print('creating chat', name, phone)
-            if text:
-                amo_chats_client = AmoChatsAPIClient(branch=branch)
-                new_chat = amo_chats_client.get_new_message(
+            if not branch:
+                # print('no branch')
+                return '200 OK HTTPS.', 200
+            contact = (value.get('contacts') or [{}])[0]
+            if not contact:
+                """
+                {'object': 'whatsapp_business_account', 'entry': [{'id': '133570986506988', 'changes': [{'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '381114221400', 'phone_number_id': '151648284687808'}, 'statuses': [{'id': 'wamid.HBgMOTk1NTkxMDU4NjE4FQIAERgSOEJDNTQzN0E1MkU2QzEwM0UwAA==', 'status': 'delivered', 'timestamp': '1701896963', 'recipient_id': '995591058618', 'conversation': {'id': '284a2cc182835831700f6711b563f032', 'origin': {'type': 'utility'}}, 'pricing': {'billable': True, 'pricing_model': 'CBP', 'category': 'utility'}}]}, 'field': 'messages'}]}]}
+                """
+                # print('uncatched WhatsApp error')
+                return '200 OK HTTPS.', 200
+            msg = value['messages'][0]
+            # print('msg', msg)
+            timestamp = int(msg['timestamp'])
+            name = contact['profile']['name']
+            phone = contact['wa_id']
+            # текста может не быть - вероятно, нам прислали файл
+            text = (msg.get('text') or {}).get('body')
+            # ищем лид и связанный с ним контакт
+            contact_id = None
+            # print('init amo api client...')
+            amo_client = API_CLIENT.get(branch)()
+            # print('searching contacts...', phone[-8:])
+            contacts = [x for x in amo_client.find_contacts(query=phone[-8:]) or []]
+            if contacts:
+                contact_id = contacts[0]['id']
+                # print('contact found!', contacts)
+            # leads = [x for x in amo_client.find_leads(query=phone[-8:])]
+            # print('leads', leads)
+            # if leads:
+            #     contacts = leads[0]['_embedded']['contacts']
+            #     if contacts:
+            #         contact_id = contacts[0]['id']
+            # если контакт существует, ищем связанные с ним чаты
+            chat_id = None
+            conversation_id = None
+            if contact_id:
+                print('trying to find chats...')
+                chats = amo_client.get_chats(contact_id=contact_id)
+                print('chats', chats)
+                if chats:
+                    chat_id = chats[-1]['chat_id']
+            else:
+                # контакта не существует, кидаем чат в "неразобранное"
+                # conversation_id = str(uuid.uuid4())
+                # print('creating chat', name, phone)
+                if text:
+                    amo_chats_client = AmoChatsAPIClient(branch=branch)
+                    new_chat = amo_chats_client.get_new_message(
+                        name=name,
+                        phone=phone,
+                        conversation_id=str(uuid.uuid4())
+                    )
+                    new_chat_id = new_chat['id']
+                    print('trying to create unsorted...', timestamp, name, phone, text, new_chat_id)
+                    amo_chats_client.get_message(
+                        timestamp=timestamp,
+                        name=name,
+                        phone=phone,
+                        text=text,
+                        conversation_id=new_chat_id,
+                        msg_id=str(uuid.uuid4())
+                    )
+                    print('ok')
+                return '200 OK HTTPS.', 200
+            # есть контакт, но нет чата - создаем новый чат и связываем его с контактом
+            if not chat_id and text:
+                print('creating chat', name, phone)
+                conversation_id = str(uuid.uuid4())
+                new_chat = AmoChatsAPIClient(branch=branch).get_new_message(
                     name=name,
                     phone=phone,
-                    conversation_id=str(uuid.uuid4())
+                    conversation_id=conversation_id
                 )
-                new_chat_id = new_chat['id']
-                print('trying to create unsorted...', timestamp, name, phone, text, new_chat_id)
-                amo_chats_client.get_message(
-                    timestamp=timestamp,
+                chat_id = new_chat['id']
+                # связываем чат с контактом
+                print('linking chat with contact', chat_id, contact_id)
+                tmp = amo_client.link_chat_with_contact(contact_id=contact_id, chat_id=chat_id)
+                print(tmp.text)
+            # пишем сообщение в чат
+            if chat_id and text:
+                print('writing msg', name, phone, text, chat_id)
+                AmoChatsAPIClient(branch=branch).get_message(
+                    timestamp=int(time.time()),  # поправка по времени?
                     name=name,
                     phone=phone,
                     text=text,
-                    conversation_id=new_chat_id,
+                    conversation_id=conversation_id or str(uuid.uuid4()),
+                    # conversation_id=chat_id,
+                    conversation_ref_id=chat_id,
                     msg_id=str(uuid.uuid4())
                 )
-                print('ok')
-            return '200 OK HTTPS.', 200
-        # есть контакт, но нет чата - создаем новый чат и связываем его с контактом
-        if not chat_id and text:
-            print('creating chat', name, phone)
-            conversation_id = str(uuid.uuid4())
-            new_chat = AmoChatsAPIClient(branch=branch).get_new_message(
-                name=name,
-                phone=phone,
-                conversation_id=conversation_id
-            )
-            chat_id = new_chat['id']
-            # связываем чат с контактом
-            print('linking chat with contact', chat_id, contact_id)
-            tmp = amo_client.link_chat_with_contact(contact_id=contact_id, chat_id=chat_id)
-            print(tmp.text)
-        # пишем сообщение в чат
-        if chat_id and text:
-            print('writing msg', name, phone, text, chat_id)
-            AmoChatsAPIClient(branch=branch).get_message(
-                timestamp=int(time.time()),  # поправка по времени?
-                name=name,
-                phone=phone,
-                text=text,
-                conversation_id=conversation_id or str(uuid.uuid4()),
-                # conversation_id=chat_id,
-                conversation_ref_id=chat_id,
-                msg_id=str(uuid.uuid4())
-            )
-        # всегда проверяем и вложения (вероятно, нам прислали какие-то файлы)
-        attachments: List[str] = WhatsAppController(branch=branch).get_attachments_from_incoming_msg(data=data)
-        print('attachments:', attachments)
-        lead_id = amo_client.get_lead_id_by_contact_id(contact_id=contact_id)
-        print('lead_id', lead_id)
-        for file in attachments:
-            amo_client.upload_file(file_path=file, lead_id=lead_id)
-        # отправляем оповещение о новом сообщении в телеграм
-        config = Config()
-        lead_data = amo_client.get_lead_by_id(lead_id=lead_id)
-        pipeline_id = lead_data.get('pipeline_id')
-        branch = {'SM': 'swissmedica', 'CDV': 'drvorobjev'}.get(branch)
-        params = config.new_lead_telegram.get(pipeline_id)
-        if params:
-            bot_key = pipeline_id
-        else:
-            params = config.new_lead_telegram.get(branch)
-            bot_key = branch
-        if not params:
-            return '200 OK HTTPS.', 200
-        domain = lead_data['_links']['self']['href'].split('https://')[1].split('.')[0]
-        # тегаем пользователя через @
-        processor = DATA_PROCESSOR.get(branch)()
-        user = processor.get_user_by_id(user_id=lead_data.get('responsible_user_id'))
-        user = user.name if user else ''
-        telegram_name = None
-        managers = GoogleAPIClient(book_id=GoogleSheets.Managers.value, sheet_title='managers').get_sheet()
-        for manager in managers:
-            if manager.get('manager') == user:
-                telegram_name = manager.get('telegram')
-                break
-        telegram_name = f"@{telegram_name}" if telegram_name else ''
-        msg = f"New WhatsApp message: https://{domain}.amocrm.ru/leads/detail/{lead_id}" \
-              f"\nResponsible: {telegram_name if telegram_name else user}".strip()
-        BOTS[bot_key].send_message(params.get('NEW_LEAD'), msg)
-    except Exception as exc:
-        print(f'WhatsApp webhook error: {exc}')
-        # todo WhatsApp webhook error: 'contacts'
-        """
-        WhatsApp response on sending msg {"messaging_product":"whatsapp","contacts":[{"input":"375292799419","wa_id":"375292799419"}],"messages":[{"id":"wamid.HBgMMzc1MjkyNzk5NDE5FQIAERgSNEEwOTI2MUFEMDI1OEVBQjIxAA=="}]}
-        2023-11-27T10:03:08.195474+00:00 app[web.1]: 10.1.37.119 - - [27/Nov/2023:10:03:08 +0000] "POST /amo_chat/3a952d6f-afb1-4154-977a-a6f2eeb2053e_59a2fb56-7492-4c16-8bbe-f776345af46c HTTP/1.1" 204 0 "-" "amoCRM amoJo/1.0"
-        """
+            # всегда проверяем и вложения (вероятно, нам прислали какие-то файлы)
+            attachments: List[str] = WhatsAppController(branch=branch).get_attachments_from_incoming_msg(data=data)
+            print('attachments:', attachments)
+            lead_id = amo_client.get_lead_id_by_contact_id(contact_id=contact_id)
+            print('lead_id', lead_id)
+            for file in attachments:
+                amo_client.upload_file(file_path=file, lead_id=lead_id)
+            # отправляем оповещение о новом сообщении в телеграм
+            config = Config()
+            lead_data = amo_client.get_lead_by_id(lead_id=lead_id)
+            pipeline_id = lead_data.get('pipeline_id')
+            branch = {'SM': 'swissmedica', 'CDV': 'drvorobjev'}.get(branch)
+            params = config.new_lead_telegram.get(pipeline_id)
+            if params:
+                bot_key = pipeline_id
+            else:
+                params = config.new_lead_telegram.get(branch)
+                bot_key = branch
+            if not params:
+                return '200 OK HTTPS.', 200
+            domain = lead_data['_links']['self']['href'].split('https://')[1].split('.')[0]
+            # тегаем пользователя через @
+            processor = DATA_PROCESSOR.get(branch)()
+            user = processor.get_user_by_id(user_id=lead_data.get('responsible_user_id'))
+            user = user.name if user else ''
+            telegram_name = None
+            managers = GoogleAPIClient(book_id=GoogleSheets.Managers.value, sheet_title='managers').get_sheet()
+            for manager in managers:
+                if manager.get('manager') == user:
+                    telegram_name = manager.get('telegram')
+                    break
+            telegram_name = f"@{telegram_name}" if telegram_name else ''
+            msg = f"New WhatsApp message: https://{domain}.amocrm.ru/leads/detail/{lead_id}" \
+                  f"\nResponsible: {telegram_name if telegram_name else user}".strip()
+            BOTS[bot_key].send_message(params.get('NEW_LEAD'), msg)
+        except Exception as exc:
+            print(f'WhatsApp webhook error: {exc}')
+            # todo WhatsApp webhook error: 'contacts'
+            """
+            WhatsApp response on sending msg {"messaging_product":"whatsapp","contacts":[{"input":"375292799419","wa_id":"375292799419"}],"messages":[{"id":"wamid.HBgMMzc1MjkyNzk5NDE5FQIAERgSNEEwOTI2MUFEMDI1OEVBQjIxAA=="}]}
+            2023-11-27T10:03:08.195474+00:00 app[web.1]: 10.1.37.119 - - [27/Nov/2023:10:03:08 +0000] "POST /amo_chat/3a952d6f-afb1-4154-977a-a6f2eeb2053e_59a2fb56-7492-4c16-8bbe-f776345af46c HTTP/1.1" 204 0 "-" "amoCRM amoJo/1.0"
+            """
 
 
 @bp.route('/whatsapp_remove', methods=['GET', 'POST'])
