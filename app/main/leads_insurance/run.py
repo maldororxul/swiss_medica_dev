327b61da56b2a1cf459cfe430d5a5eca072674c4
maldororxul <studia-kirill@yandex.ru>
2024-02-14 00:16:58 +0400

    spam filter

--- app/main/leads_insurance/run.py
+++ app/main/leads_insurance/run.py
@@ -1,3 +1,3 @@
 def run(*args):
-    from app.main.leads_insurance.handler import start_autocall_iteration
-    start_autocall_iteration(*args)
+    from app.main.leads_insurance.handler import start_leads_insurance_iteration
+    start_leads_insurance_iteration(*args)
