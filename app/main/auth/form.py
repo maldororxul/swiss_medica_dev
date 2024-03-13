d80ba3dbf502b5cf39ae6ea2b5c6b7458f766938
maldororxul <studia-kirill@yandex.ru>
2024-03-13 15:35:31 +0400

    auth logic

--- app/main/auth/form.py
+++ app/main/auth/form.py
@@ -1,10 +1,17 @@
 """ Форма регистрации """
+import re
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms import StringField, PasswordField, ValidationError
+from wtforms.validators import InputRequired, Length
+
+
+def email_validator(form, field):
+    # шаблон для проверки формата email
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", field.data):
+        raise ValidationError("Invalid email address.")
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=80)])
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=120)])
+    email = StringField('Email', validators=[InputRequired(), email_validator, Length(max=120)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8)])
