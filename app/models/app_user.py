9274721a013d8afeb973453c8b63f21469335956
maldororxul <studia-kirill@yandex.ru>
2024-03-13 18:15:16 +0400

    auth logic

--- app/models/app_user.py
+++ app/models/app_user.py
@@ -1,5 +1,6 @@
 """ Пользователь приложения """
 __author__ = 'ke.mizonov'
+from sqlalchemy.orm import declared_attr
 from app.extensions import db
 
 
@@ -18,15 +19,23 @@ class AppUserBase(db.Model):
     password_hash = db.Column(db.String(128))
     is_active = db.Column(db.Boolean, default=True)
     is_authenticated = db.Column(db.Boolean, default=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
-    role = db.relationship('Role', backref=db.backref('users', lazy=True))
+
+    @declared_attr
+    def role_id(cls):
+        return db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    @declared_attr
+    def role(cls):
+        return db.relationship('Role', backref='users')
 
 
 class SMAppUser(AppUserBase):
     __tablename__ = 'app_user'
     __table_args__ = {"schema": "sm"}
+    __mapper_args__ = {'polymorphic_identity': 'sm'}
 
 
 class CDVAppUser(AppUserBase):
     __tablename__ = 'app_user'
     __table_args__ = {"schema": "cdv"}
+    __mapper_args__ = {'polymorphic_identity': 'cdv'}
