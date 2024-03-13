57a194d0d2bb8a605e34a61c6bf046bcc4e1eb8e
maldororxul <studia-kirill@yandex.ru>
2024-03-13 17:01:43 +0400

    auth logic

--- app/models/app_user.py
+++ app/models/app_user.py
@@ -11,6 +11,7 @@ class AppUserBase(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     is_active = db.Column(db.Boolean, default=True)
+    is_authenticated = db.Column(db.Boolean, default=True)
 
     def __repr__(self):
         return f'<Application user "{self.username}">'
