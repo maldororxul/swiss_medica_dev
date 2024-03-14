abf227ca11d2c2360b597797cd52943796022b82
maldororxul <studia-kirill@yandex.ru>
2024-03-14 10:42:44 +0400

    auth logic

--- app/models/app_user.py
+++ app/models/app_user.py
@@ -4,6 +4,7 @@ from app.extensions import db
 
 
 class RoleBase(db.Model):
+    __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
 
